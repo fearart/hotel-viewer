419a5fcb0a88c49843646c9c497ba429e74a5d9f
niklasduo <duoniklas@gmail.com>
2023-12-13 08:11:37 +0100

    Refactor Telegram integration

--- server/plugins/telegram.ts
+++ server/plugins/telegram.ts
@@ -1,7 +1,7 @@
 import { Nitro } from "nitropack";
-import bot from "~/utilities/bot";
+import { Telegraf } from "telegraf";
 export default async (_nitroApp: Nitro) => {
   const config = useRuntimeConfig();
-
+  const bot = new Telegraf(config.telegram_token);
   bot.launch()
 };
--- utilities/bot.ts
+++
@@ -1,5 +0,0 @@
-import { Telegraf } from "telegraf";
-
-const bot = new Telegraf('6581985549:AAFIVOw-49ZgbxDIxHoKLAlvH6HOgGlLWOM');
-
-export default bot;
