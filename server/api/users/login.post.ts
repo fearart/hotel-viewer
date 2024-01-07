559c51634d0a57580aa4a342c2a401db44faebb1
Mykola <duoniklas@gmail.com>
2024-01-07 21:38:53 +0100

    fixed token burning

--- server/api/users/login.post.ts
+++ server/api/users/login.post.ts
@@ -12,6 +12,7 @@ const missingDataReturnMessage = {
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
+    const config = useRuntimeConfig()
     if (typeof(body.login) === 'undefined') { return missingDataReturnMessage}
     if (typeof(body.password) === 'undefined') {return missingDataReturnMessage}
     const user = await mongoose.connection.db.collection('hotel-users').findOne({"login" : body.login})
@@ -19,12 +20,25 @@ export default defineEventHandler(async (event) => {
         setResponseStatus(event,203,"Wrong password or email")
         return
     }
-
     const result: boolean = await bcrypt.compare(body.password,user.password)
     if (result === false) {
         setResponseStatus(event,203,"Wrong password or email")
         return
     }
+    try {
+        let token = jwt.verify(user.token,config.jwt_secret)
+    }
+    catch (e) {
+        let newToken = jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30),
+            data: body.email
+            },
+            config.jwt_secret
+        )
+        user.token = newToken
+        await mongoose.connection.db.collection('hotel-users').updateOne({"login" : body.login},{$set:{"token":newToken}})
+    }
+    
     setResponseStatus(event,202,"OK")
     setCookie(event,'token',user.token)
 })
